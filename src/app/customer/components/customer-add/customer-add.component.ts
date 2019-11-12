fa98c732e3874f50904a25f6e981ab3115073a76
Hüseyin Soytürk (ETIYA) <huseyin.soyturk@etiya.com>
2019-11-12 10:58:18 +0300

    customer add

--- src/app/customer/components/customer-add/customer-add.component.ts
+++ src/app/customer/components/customer-add/customer-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducer';
+import { Customer } from '../../store/customer.model';
+import { AddCustomer } from '../../store/customer.action';
 
 @Component({
   selector: 'app-customer-add',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add.component.scss']
 })
 export class CustomerAddComponent implements OnInit {
+  subs: Subscription
+  nextIndex: number
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subs = this.store.select('customerState').subscribe(res => {
+      this.nextIndex = res.nextIndex;
+    })
+  }
+
+  onClickAdd(name, surname, tel) {
+    const customer: Customer = {
+      id: this.nextIndex,
+      name,
+      surname,
+      tel
+    }
+    this.store.dispatch(new AddCustomer(customer))
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
 }

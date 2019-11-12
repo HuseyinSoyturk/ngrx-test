fcdecaa1833e0dfa94998b5421225f16f1cc722d
Hüseyin Soytürk <huseyin.soyturk@etiya.com>
2019-11-12 18:24:11 +0300

    rent components create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,33 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { RouterModule, Routes } from '@angular/router';
 
+import { environment } from '../environments/environment';
+
+import * as fromApp from './store/app.reducer';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RootComponent } from './root/root.component';
+
 import { BookComponent } from './book/book.component'
 import { BookListComponent } from './book/components/book-list/book-list.component';
 import { BookAddComponent } from './book/components/book-add/book-add.component';
 import { BookEditComponent } from './book/components/book-edit/book-edit.component';
-import * as fromApp from './store/app.reducer';
-import { HeaderComponent } from './header/header.component';
-import { RootComponent } from './root/root.component';
-import { environment } from '../environments/environment';
+
+import { CustomerComponent } from './customer/customer.component';
 import { CustomerListComponent } from './customer/components/customer-list/customer-list.component';
 import { CustomerAddComponent } from './customer/components/customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer/components/customer-edit/customer-edit.component';
-import { CustomerComponent } from './customer/customer.component';
+
+import { RentComponent } from './rent/rent.component';
+import { RentListComponent } from './rent/components/rent-list/rent-list.component';
+import { RentAddComponent } from './rent/components/rent-add/rent-add.component';
+import { RentEditComponent } from './rent/components/rent-edit/rent-edit.component';
 
 const appRoutes: Routes = [
   { path: 'book', component: BookComponent },
   { path: 'customer', component:CustomerComponent  },
-  // { path: 'rent', component: RentComponent }
+  { path: 'rent', component: RentComponent }
 ];
 
 @NgModule({
@@ -37,6 +46,10 @@ const appRoutes: Routes = [
     CustomerAddComponent,
     CustomerEditComponent,
     CustomerComponent,
+    RentComponent,
+    RentEditComponent,
+    RentAddComponent,
+    RentListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rent/components/rent-add/rent-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rent-add',
+  templateUrl: './rent-add.component.html',
+  styleUrls: ['./rent-add.component.scss']
+})
+export class RentAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

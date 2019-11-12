070677afa90b0f26a34acd5ef96e5de7c41bedc2
Hüseyin Soytürk (ETIYA) <huseyin.soyturk@etiya.com>
2019-11-12 11:12:55 +0300

    customer edit

--- src/app/customer/components/customer-edit/customer-edit.component.ts
+++ src/app/customer/components/customer-edit/customer-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../store/customer.model';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducer';
+import { EditCustomer } from '../../store/customer.action';
 
 @Component({
   selector: 'app-customer-edit',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerEditComponent implements OnInit {
 
-  constructor() { }
+  editingCustomer: Customer;
+  subs: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subs = this.store.select('customerState').subscribe(res => {
+      this.editingCustomer = res.editingCustomer
+    })
+  }
+
+  onClickEdit(name, surname, tel) {
+    let newCustomer = {
+      ...this.editingCustomer,
+      name,
+      surname,
+      tel
+    }
+    this.store.dispatch(new EditCustomer(newCustomer))
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe()
   }
 
 }
